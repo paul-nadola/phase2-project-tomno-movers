a26e6126d87f2448c87a8af252a80bee1c76cfe8
Paul Nadola Oyinga <nadolapaul@gmail.com>
2023-05-02 11:39:03 +0300

    Header

---
+++ src/Header.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div id='header'>
+        <div id='header-name'>
+             <h1>TOMNO MOVERS</h1>
+            <p>Big or small… We can handle it all.</p>
+        </div>
+        <div>
+            <form id='search'>
+                <label htmlFor="type-search">SEARCH BY TYPE</label>
+                <input type="text" placeholder='Search by type' />
+               <br />
+               <label htmlFor="category-search">SEARCH BY CATEGORY</label>
+                <input id='category-search' type="text" placeholder='Search by category' />
+
+            </form>
+        </div>
+           
+             
+        
+        {/* <form id='search'>
+            <input type="text" placeholder='Search by type' />
+            <input type="text" placeholder='Search by category' />
+
+        </form> */}
+    </div>
+  )
+}
+
+export default Header
