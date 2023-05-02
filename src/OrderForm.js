eff10585a08a680a439702eebcca984c712093b1
Paul Nadola Oyinga <nadolapaul@gmail.com>
2023-05-02 14:22:10 +0300

    Order form clean up

--- src/OrderForm.js
+++ src/OrderForm.js
@@ -1,42 +1,4 @@
-// import React, { useState } from 'react'
 
-
-// function OrderForm() {
-//     const [range, setRange] = useState(0)
-//     function handleChange(e){
-//         setRange(e.target.value)
-//     }
-//   return (
-//     <form id='order-form'>
-//         <p>Total price : KSH </p>
-//         <label htmlFor="type-options">Choose the type of premises you have</label>
-//         <select name="type" id="type-options">
-//             <option >STUDIO APARTMENT</option>
-//             <option >ONE BEDROOM APARTMENT</option>
-//             <option >TWO BEDROOM APARTMENT</option>
-//             <option >COMMERCIAL PREMISES</option>
-//         </select>
-//         <label htmlFor="category-options">Choose a package</label>
-//         <select name="category" id="category-options">
-//             <option >SILVER</option>
-//             <option >GOLD</option>
-//             <option >PLATINUM</option>
-//         </select>
-//         <label htmlFor="range">Slide to choose estimated distance in kilometres</label>
-//         <input id='range' type="range" name='range' min= {0} max= {500} 
-//         onChange={handleChange}/>
-//         <p>{range} Kilometres</p>
-//         <label htmlFor="location-to">Pick-up</label>
-//         <input type="text" id='location-from' />
-//         <label htmlFor="location-to">Destination</label>
-//         <input type="text" id='location-to' />
-//         <label htmlFor="btn">Check your order before submission</label>
-//         <button type='submit' className='btn'>Create Order</button>
-//     </form>
-//   )
-// }
-
-// export default OrderForm
 import React, { useState } from 'react'
 
 function OrderForm() {
@@ -67,24 +29,24 @@ function OrderForm() {
     // Prices per km based on premises type and package
     const prices = {
       'STUDIO APARTMENT': {
-        'SILVER': 10,
-        'GOLD': 15,
-        'PLATINUM': 20,
+        'SILVER': 200,
+        'GOLD': 250,
+        'PLATINUM': 300,
       },
       'ONE BEDROOM APARTMENT': {
-        'SILVER': 12,
-        'GOLD': 18,
-        'PLATINUM': 24,
+        'SILVER': 250,
+        'GOLD': 300,
+        'PLATINUM': 350,
       },
       'TWO BEDROOM APARTMENT': {
-        'SILVER': 15,
-        'GOLD': 22,
-        'PLATINUM': 30,
+        'SILVER': 300,
+        'GOLD': 350,
+        'PLATINUM': 400,
       },
       'COMMERCIAL PREMISES': {
-        'SILVER': 20,
-        'GOLD': 30,
-        'PLATINUM': 40,
+        'SILVER': 400,
+        'GOLD': 500,
+        'PLATINUM': 600,
       },
     }
 
